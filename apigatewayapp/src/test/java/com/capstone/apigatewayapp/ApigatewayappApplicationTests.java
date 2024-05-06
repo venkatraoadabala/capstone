9d0086fb4fd35e10853c77499cc3606d600f6a00
venkatraoadabala <162993730+venkatraoadabala@users.noreply.github.com>
2024-05-06 11:45:37 +0530

    Add files via upload

---
+++ apigatewayapp/src/test/java/com/capstone/apigatewayapp/ApigatewayappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.apigatewayapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
